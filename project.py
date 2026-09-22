d504ae346073e4a8f5d03fa1eeb2312e5dfcca89
1621COn <147944314+1621COn@users.noreply.github.com>
2026-09-21 21:49:48 -0400

    Add JSON parsing for secret logs

---
+++ project.py
@@ -0,0 +1,12 @@
+import json 
+
+filename = "secret-logs.json"
+# function to parse JSON file
+def parse_read(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)
+        return data
+logs = parse_read(filename)
+user = logs["username"]
+passw = logs["password"]
+print(f"username:{user}", f"\npassword:{passw}")
